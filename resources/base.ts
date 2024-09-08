d48280129d6ff3b14c37287102164d25f4bbc1ee
tongari <info@tongari-as.com>
2024-09-08 20:16:43 +0900

    Setting workflows

---
+++ resources/base.ts
@@ -0,0 +1,4 @@
+export type BaseType = {
+  name: string;
+  age: number;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
+import type { BaseType } from "@/base";
+
+const baseData: BaseType = {
+  name: "tongari",
+  age: 99,
+};
+
 export default function Home() {
+  console.log(baseData);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
